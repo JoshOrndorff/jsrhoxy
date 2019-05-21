5f2861fc2e27b5d8fa7788ade78caf4d6412e119
Joshy Orndorff <admin@joshyorndorff.com>
2019-05-21 11:17:33 +0200

    First step toward typescript.

--- src/jankyCrypto.ts
+++ src/jankyCrypto.ts
@@ -2,11 +2,14 @@
 // They should not be considered secure. If jsRhoxy becomes
 // serious enough that security is desired, these function should
 // be replaced. I've tried to isolate the jank to this file.
+
 module.exports = {
-    qdHash: qdHash,
-    mergeRandom: mergeRandom,
-    splitRandom: splitRandom
-};
+  qdHash,
+  mergeRandom,
+  splitRandom,
+}
+
+
 /**
  * A quick and dirty hash function that will hash anything JSONy.
  * Based on java's string hash and this artcle
@@ -14,18 +17,20 @@ module.exports = {
  * @param data Any JSON-able data to be hashed
  * @return an integer hash
  */
-function qdHash(data) {
-    var strData = JSON.stringify(data);
-    var hash = 0;
-    if (strData.length == 0)
-        return hash;
-    for (var i = 0; i < strData.length; i++) {
-        var char = strData.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash; // Convert to 32bit integer
-    }
-    return hash;
+function qdHash(data: any): number {
+  const strData = JSON.stringify(data);
+  let hash = 0;
+  if (strData.length == 0) return hash;
+  for (let i = 0; i < strData.length; i++) {
+    let char = strData.charCodeAt(i);
+    hash = ((hash<<5)-hash)+char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
 }
+
+
+
 /**
  * Poor man's  mergeable random state. Merges many random
  * states (integers) into one.
@@ -34,13 +39,15 @@ function qdHash(data) {
  * @return A new pseudo-random-ish state
  */
 function mergeRandom(initials) {
-    var merged = 0;
-    for (var _i = 0, initials_1 = initials; _i < initials_1.length; _i++) {
-        var initial = initials_1[_i];
-        merged = qdHash(merged) & qdHash(initial);
-    }
-    return qdHash(merged);
+
+  let merged = 0;
+  for (let initial of initials) {
+    merged = qdHash(merged) & qdHash(initial)
+  }
+
+  return qdHash(merged);
 }
+
 /**
  * Given an initial random state (integer), calculates and
  * returns the desired number of new deterministic random states.
@@ -50,11 +57,11 @@ function mergeRandom(initials) {
  * @return An array of new random states
  */
 function splitRandom(initial, n) {
-    var tempRandom = initial;
-    var results = [];
-    for (var i = 0; i < n; i++) {
-        tempRandom = qdHash(tempRandom);
-        results.push(tempRandom);
-    }
-    return results;
+  let tempRandom = initial;
+  let results = [];
+  for (let i = 0; i < n; i++) {
+    tempRandom = qdHash(tempRandom);
+    results.push(tempRandom);
+  }
+  return results;
 }
